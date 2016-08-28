01b3beb3f29964f01c36a97806baf57001cbdaf6
Michael <mbg@outlook.com>
2016-08-29 00:36:49 +0100

    Switched code to make it easier to submit URL

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const baseUrl = require("./baseUrl"),
     getCase = require("./getCase"),
     summariseCase = require("./summariseCase"),
     emojifySummary = require("./emojifySummary"),
-    cfg = require('./config/config'),
     async = require("async"),
-    Twitter = require("twitter"),
     _ = require('underscore'),
+    cfg = require('./config/config'),
+    Twitter = require("twitter"),
     client = new Twitter({
         consumer_key: cfg.consumerkey,
         consumer_secret: cfg.consumerSecret,
@@ -15,38 +15,45 @@ const baseUrl = require("./baseUrl"),
         access_token_secret: cfg.accessSecret   
     });
 
-getEmojiSummary(function(err, results){
-    if (err)
-        console.log(err);
-    var status = results.caseUrl + " " + results.emojiSummary;
+provideCaseUrl(baseUrl(), function(err, url){
+    getEmojiSummary(url, function(err, results){
+        postTweet(url, results.emojiSummary, function(err, res){
+            if (err)    
+                console.log(err);
+            else        
+                console.log("Tweet posted");
+        })
+    });
+})
+
+function postTweet(url, content, fn){
+    var status = url + " " + content;
     var status = status.slice(0, 140);
     client.post('statuses/update', {status: status},  function(err, tweet, response) {
-        if (err)    
-            console.log(err);
-        else        
-            console.log("Tweet posted");
-    });
-});
+        if (err)
+            fn(err)
+        else
+            fn(null, response);
+        });
+}
 
-function getEmojiSummary(fn){
+function getEmojiSummary(url, fn){
     async.auto({
-        caseUrl: function(cb){
-            provideCaseUrl(baseUrl(), cb);
-        },
-        caseDetails: ['caseUrl', function(results, cb){
-            getCase(results.caseUrl, cb);
+        caseDetails: [function(cb){
+            getCase(url, cb);
         }],
-        caseSummary: ['caseUrl', 'caseDetails', function(results, cb){
+        caseSummary: ['caseDetails', function(results, cb){
             summariseCase(results.caseDetails, cb);
         }],
-        emojiSummary: ['caseUrl', 'caseDetails', 'caseSummary', function(results, cb){
+        emojiSummary: ['caseDetails', 'caseSummary', function(results, cb){
             emojifySummary(results.caseSummary, cb);
         }]
-        }, function(err, results){
-                if (err)
-                    fn(err);
-                else
-                    fn(null, results);
-        })
+        }, 
+    function(err, results){
+        if (err)
+            fn(err);
+        else
+            fn(null, results);
+    })
 }
 
