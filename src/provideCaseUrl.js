f106e554e444a85352bda351a15cf053aabeb998
Michael <mbg@outlook.com>
2018-05-14 13:53:14 +0100

    Update provideCaseUrl.js

--- src/provideCaseUrl.js
+++ src/provideCaseUrl.js
@@ -4,13 +4,11 @@ import request from 'async-request'
 
 const provideCaseUrl = async url => {
   let { body } = await request(url)
-  return new Promise(resolve => {
-    let $ = cheerio.load(body)
-    let urlArray = $('ul').find('li').find('a').map(function (index, el) {
-      return $(this).attr('href')
-    })
-    resolve(`http://www.bailii.org${_.sample(_.values(urlArray))}`)
+  let $ = cheerio.load(body)
+  let urlArray = $('ul').find('li').find('a').map(function (index, el) {
+    return $(this).attr('href')
   })
+  return `http://www.bailii.org${_.sample(_.values(urlArray))}`
 }
 
 export default provideCaseUrl
