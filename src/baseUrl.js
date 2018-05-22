48c008928b3e8b2347d557a469b2acdfeafc84d2
Michael <mbg@outlook.com>
2018-05-22 23:44:35 +0100

    Update to cron

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,7 +1,7 @@
 import _ from 'lodash'
 
 const baseUrl = () => {
-  var num = _.random(0, 660).toString()
+  var num = _.random(0, 650).toString()
   if (num.length === 1) num = `000${num}`
   if (num.length === 2) num = `00${num}`
   if (num.length === 3) num = `0${num}`
--- src/cron.js
+++ src/cron.js
@@ -1,11 +1,11 @@
 import cron from 'node-cron'
 
-cron.schedule('* 10 * * *', () => {
-  '/usr/local/bin/node /mnt/c/Users/mbg/Documents/case-law-emoji-bot/compiled/tweetRandomCase'
+cron.schedule('/1 * * * *', () => {
+  '/usr/bin/node /mnt/c/Users/mbg/Documents/case-law-emoji-bot/compiled/tweetRandomCase'
 })
 
 cron.schedule('*/5 * * * *', () => {
-  '/usr/local/bin/node /mnt/c/Users/mbg/Documents/case-law-emoji-bot/compiled/tweetMentionedCase'
+  '/usr/bin/node /mnt/c/Users/mbg/Documents/case-law-emoji-bot/compiled/tweetMentionedCase'
 })
 
 cron.schedule('*/1 * * * *', () => {
